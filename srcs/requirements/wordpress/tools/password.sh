65941432c83057ad928d1677d537edab33b973a8
David P <89156264+Rel09@users.noreply.github.com>
2024-03-14 04:24:57 -0400

    Password Policy v2

--- srcs/requirements/wordpress/tools/password.sh
+++ srcs/requirements/wordpress/tools/password.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ "$(echo "${WP_ADMIN_USR}" | tr "[:upper:]" "[:lower:]")" != "${WP_ADMIN_USR}" ] || [ "$(echo "${WP_ADMIN_USR}" | grep -i "admin")" ]; then
+if echo "${WP_ADMIN_USR}" | tr '[:upper:]' '[:lower:]' | grep -q 'admin'; then
    echo "--- PASSWORD SHOULD NOT CONTAIN ADMIN ---"
    echo "--- ADMIN ACCOUNT SET TO 'LORD_BOZO' ----"
    export WP_ADMIN_USR="LORD_BOZO"
